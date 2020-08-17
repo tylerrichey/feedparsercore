6e7f094fd4c4dcdbdb57dccd40eecf0786db2fe3
Tyler Richey <iam@tylerrichey.com>
2020-08-17 07:24:20 -0700

    async

--- FeedParserCore/FeedParser.cs
+++ FeedParserCore/FeedParser.cs
@@ -2,6 +2,10 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Xml.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.IO;
 
 namespace FeedParserCore
 {
@@ -10,44 +14,47 @@ namespace FeedParserCore
     /// </summary>
     public static class FeedParser
     {
+        private static HttpClient httpClient = new HttpClient();
+
         /// <summary>
         /// Parses the given <see cref="FeedType"/> and returns a <see cref="IEnumerable<FeedItem>"/>.
         /// </summary>
         /// <returns></returns>
-        public static IEnumerable<FeedItem> Parse(string url, FeedType feedType, bool throwExceptions = false)
-        {
-            switch (feedType)
+        public static async Task<IEnumerable<FeedItem>> ParseAsync(string url, FeedType feedType, bool throwExceptions = false) =>
+            feedType switch
             {
-                case FeedType.RSS:
-                    return ParseRss(url, throwExceptions);
-                case FeedType.RDF:
-                    return ParseRdf(url, throwExceptions);
-                case FeedType.Atom:
-                    return ParseAtom(url, throwExceptions);
-                default:
-                    throw new NotSupportedException(string.Format("{0} is not supported", feedType.ToString()));
-            }
-        }
+                FeedType.RSS => await ParseRssAsync(url, throwExceptions),
+                FeedType.RDF => await ParseRdfAsync(url, throwExceptions),
+                FeedType.Atom => await ParseAtomAsync(url, throwExceptions),
+                _ => throw new NotSupportedException(string.Format("{0} is not supported", feedType.ToString()))
+            };
 
-		/// <summary>
-		/// Parses an Atom feed and returns a <see cref="IEnumerable<FeedItem>"/>.
-		/// </summary>
-		private static IEnumerable<FeedItem> ParseAtom(string url, bool throwExceptions)
+        /// <summary>
+        /// Parses the given <see cref="FeedType"/> and returns a <see cref="IEnumerable<FeedItem>"/>.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<FeedItem> Parse(string url, FeedType feedType, bool throwExceptions = false) => ParseAsync(url, feedType, throwExceptions).Result;
+
+        /// <summary>
+        /// Parses an Atom feed and returns a <see cref="IEnumerable<FeedItem>"/>.
+        /// </summary>
+        private static async Task<IEnumerable<FeedItem>> ParseAtomAsync(string url, bool throwExceptions)
         {
             try
             {
-                return XDocument.Load(url)
-                                .Root
-                                .Elements()
-                                .Where(i => i.Name.LocalName == "entry")
-                                .Select(item => new FeedItem
-                                {
-                                    FeedType = FeedType.Atom,
-                                    Content = item.GetElementValue<string>("content"),
-                                    Link = item.GetElementValue<string>("link", "href"),
-                                    PublishDate = item.GetElementValue<DateTime>("updated"),
-                                    Title = item.GetElementValue<string>("title")
-                                });
+                var xDocument = await GetXDocument(url);
+                return xDocument
+                    .Root
+                    .Elements()
+                    .Where(i => i.Name.LocalName == "entry")
+                    .Select(item => new FeedItem
+                    {
+                        FeedType = FeedType.Atom,
+                        Content = item.GetElementValue<string>("content"),
+                        Link = item.GetElementValue<string>("link", "href"),
+                        PublishDate = item.GetElementValue<DateTime>("updated"),
+                        Title = item.GetElementValue<string>("title")
+                    });
             }
             catch (Exception e)
             {
@@ -58,24 +65,25 @@ namespace FeedParserCore
 		/// <summary>
 		/// Parses an RSS feed and returns a <see cref="IEnumerable<FeedItem>"/>.
 		/// </summary>
-		private static IEnumerable<FeedItem> ParseRss(string url, bool throwExceptions)
+		private static async Task<IEnumerable<FeedItem>> ParseRssAsync(string url, bool throwExceptions)
         {
             try
             {
-                return XDocument.Load(url)
-                         .Root
-                         .Descendants()
-                         .Where(i => i.Name.LocalName == "channel")
-                         .Elements()
-                         .Where(i => i.Name.LocalName == "item")
-                         .Select(item => new FeedItem
-                         {
-                             FeedType = FeedType.RSS,
-                             Content = item.GetElementValue<string>("description"),
-                             Link = item.GetElementValue<string>("link"),
-                             PublishDate = item.GetElementValue<DateTime>("pubDate"),
-                             Title = item.GetElementValue<string>("title")
-                         });
+                var xDocument = await GetXDocument(url);
+                return xDocument
+                    .Root
+                    .Descendants()
+                    .Where(i => i.Name.LocalName == "channel")
+                    .Elements()
+                    .Where(i => i.Name.LocalName == "item")
+                    .Select(item => new FeedItem
+                    {
+                        FeedType = FeedType.RSS,
+                        Content = item.GetElementValue<string>("description"),
+                        Link = item.GetElementValue<string>("link"),
+                        PublishDate = item.GetElementValue<DateTime>("pubDate"),
+                        Title = item.GetElementValue<string>("title")
+                    });
             }
             catch (Exception e)
             {
@@ -86,22 +94,23 @@ namespace FeedParserCore
 		/// <summary>
 		/// Parses an RDF feed and returns a <see cref="IEnumerable<FeedItem>"/>.
 		/// </summary>
-		private static IEnumerable<FeedItem> ParseRdf(string url, bool throwExceptions)
+		private static async Task<IEnumerable<FeedItem>> ParseRdfAsync(string url, bool throwExceptions)
         {
             try
             {
-                return XDocument.Load(url)
-                                .Root
-                                .Descendants()
-                                .Where(i => i.Name.LocalName == "item")
-                                .Select(item => new FeedItem
-                                {
-                                    FeedType = FeedType.RDF,
-                                    Content = item.GetElementValue<string>("description"),
-                                    Link = item.GetElementValue<string>("link"),
-                                    PublishDate = item.GetElementValue<DateTime>("date"),
-                                    Title = item.GetElementValue<string>("title")
-                                });
+                var xDocument = await GetXDocument(url);
+                return xDocument
+                    .Root
+                    .Descendants()
+                    .Where(i => i.Name.LocalName == "item")
+                    .Select(item => new FeedItem
+                    {
+                        FeedType = FeedType.RDF,
+                        Content = item.GetElementValue<string>("description"),
+                        Link = item.GetElementValue<string>("link"),
+                        PublishDate = item.GetElementValue<DateTime>("date"),
+                        Title = item.GetElementValue<string>("title")
+                    });
             }
             catch (Exception e)
             {
@@ -109,9 +118,25 @@ namespace FeedParserCore
             }
         }
 
+        private static async Task<XDocument> GetXDocument(string url)
+        {
+            Stream data;
+            if (Uri.TryCreate(url, UriKind.Absolute, out Uri result))
+            {
+                data = await httpClient.GetStreamAsync(url);
+            }
+            else
+            {
+                data = File.OpenRead(url);
+            }
+            //LoadAsync is only in .net standard 2.1
+            return await Task.Run(() => XDocument.Load(data));
+        }
+
         private static T GetElementValue<T>(this XElement element, string fieldName, string attribute = null)
         {
-            var item = element.Elements().FirstOrDefault(i => i.Name.LocalName == fieldName);
+            var item = element.Elements()
+                .FirstOrDefault(i => i.Name.LocalName == fieldName);
 
             if (item == null)
             {
@@ -135,34 +160,12 @@ namespace FeedParserCore
             }
         }
 
-        private static IEnumerable<FeedItem> HandleException(bool throwExceptions, Exception e)
-        {
-            if (throwExceptions)
-            {
-                throw e;
-            }
-            else
-            {
-                return new List<FeedItem>();
-            }
-        }
-    }
-    /// <summary>
-    /// Represents the XML format of a feed.
-    /// </summary>
-    public enum FeedType
-    {
+        private static IEnumerable<FeedItem> HandleException(bool throwExceptions, Exception e) => throwExceptions ? throw e : new List<FeedItem>();
+
         /// <summary>
-        /// Really Simple Syndication format.
+        /// Pass in the HttpClient instance your application is already using.
         /// </summary>
-        RSS,
-        /// <summary>
-        /// RDF site summary format.
-        /// </summary>
-        RDF,
-        /// <summary>
-        /// Atom Syndication format.
-        /// </summary>
-        Atom
+        /// <param name="newHttpClient"></param>
+        public static void SetHttpClient(HttpClient newHttpClient) => httpClient = newHttpClient;
     }
 }
---
+++ FeedParserCore/FeedType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace FeedParserCore
+{
+    /// <summary>
+    /// Represents the XML format of a feed.
+    /// </summary>
+    public enum FeedType
+    {
+        /// <summary>
+        /// Really Simple Syndication format.
+        /// </summary>
+        RSS,
+        /// <summary>
+        /// RDF site summary format.
+        /// </summary>
+        RDF,
+        /// <summary>
+        /// Atom Syndication format.
+        /// </summary>
+        Atom
+    }
+}
